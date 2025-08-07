99fd85a97feaa26e8f9a929e0db97aa2b01d2ec5
Pedro J. De la Rosa <informaticopjrg@gmail.com>
2025-08-07 00:30:58 -0400

    Subiendo juego Realidad Virtual

---
+++ Assets/Scrips/AutoCaminar.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class AutoCaminar : MonoBehaviour
+{
+    public GameObject VisionVR;
+    public const int AnguloRecto = 90;
+    public bool EstaCaminando = false;
+    public float Velocidad;
+    public bool CaminarCuanddoPulsamos;
+    public bool CaminarCuandoMiramos;
+    public double AnguloDelUmbral;
+    public bool CongelarLaPosicionY;
+    public float CompensarY;
+
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        if (controller == null)
+            Debug.LogError("Falta el CharacterController en el GameObject");
+    }
+
+    void Update()
+    {
+        if (CaminarCuandoMiramos && !CaminarCuanddoPulsamos && !EstaCaminando && VisionVR.transform.eulerAngles.x >= AnguloDelUmbral && VisionVR.transform.eulerAngles.x <= AnguloRecto)
+        {
+            EstaCaminando = true;
+        }
+        else if (CaminarCuandoMiramos && !CaminarCuanddoPulsamos && EstaCaminando && (VisionVR.transform.eulerAngles.x <= AnguloDelUmbral || VisionVR.transform.eulerAngles.x >= AnguloRecto))
+        {
+            EstaCaminando = false;
+        }
+
+        if (EstaCaminando)
+        {
+            Caminar();
+        }
+
+        if (CongelarLaPosicionY)
+        {
+            Vector3 pos = transform.position;
+            pos.y = CompensarY;
+            transform.position = pos;
+        }
+    }
+
+    private void Caminar()
+    {
+        // Dirección solo horizontal, sin componente Y
+        Vector3 direccion = new Vector3(VisionVR.transform.forward.x, 0, VisionVR.transform.forward.z).normalized;
+
+        // Multiplicamos por velocidad y deltaTime
+        Vector3 movimiento = direccion * Velocidad * Time.deltaTime;
+
+        // Mover con CharacterController para que respete colisiones y suelo
+        controller.Move(movimiento);
+    }
+}
---
+++ Assets/Scrips/ControllerVRPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Rigidbody))]
+public class ControllerVRPlayer : MonoBehaviour
+{
+    private CharacterController ControladorDelPersonaje;
+    private Vector3 MovimientoEnDireccion = Vector3.zero;
+    private Vector2 Entrada;
+
+    private CollisionFlags BanderasDeColision;
+
+    public float FuerzaAlTocarElSuelo;
+    public float MultiplicarGravedad;
+    void Start()
+    {
+        ControladorDelPersonaje = GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 MovimientoDeseado = transform.forward * Entrada.y + transform.right * Entrada.x;
+
+        RaycastHit histInfo;
+        Physics.SphereCast(transform.position, ControladorDelPersonaje.radius, Vector3.down, out histInfo, 
+                    ControladorDelPersonaje.height / 2f, Physics.AllLayers, QueryTriggerInteraction.Ignore);
+
+        MovimientoDeseado = Vector3.ProjectOnPlane(MovimientoDeseado, histInfo.normal).normalized;
+
+        if (ControladorDelPersonaje.isGrounded)
+        {
+            MovimientoEnDireccion.y = -FuerzaAlTocarElSuelo;
+        }
+        else
+        {
+            MovimientoEnDireccion += Physics.gravity * MultiplicarGravedad * Time.fixedDeltaTime;
+        }
+
+        BanderasDeColision = ControladorDelPersonaje.Move(MovimientoEnDireccion * Time.fixedDeltaTime);
+    }
+}
